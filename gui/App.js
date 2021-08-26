b27706258753a7128ef427da340f0890270ef80c
nuernie <nerdhub@web.de>
2021-08-26 12:29:05 +0200

    v 0.0.0

--- gui/App.js
+++ gui/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>!Master!</Text>
+      <Text>!Master new!</Text>
       <StatusBar style="auto" />
     </View>
   );
