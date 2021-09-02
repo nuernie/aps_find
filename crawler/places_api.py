c848f6f2e36013e09a01059b7a408980d4247f07
nuernie <nerdhub@web.de>
2021-09-02 07:59:47 +0200

    v 0.0.0

--- crawler/places_api.py
+++ crawler/places_api.py
@@ -1,54 +1,62 @@
-import traceback
-
 import requests
-import json
-import sys
 import pandas as pd
 
 # TODO bei Serverproblemen neue starten nach zeit ablauf !
 # TODO Overpass API Limits abchecken
-# http://overpass-api.de/api/interpreter?data=[out:json];area[name="München"];nwr(area)[amenity~"^(bar|cafe|pub|restaurant)$"];out center;
-# {} dict in python / Zugriff .get("")
-# [] list in python / Zugriff [0]
-
-struct = {}
-overpass_url = "http://overpass-api.de/api/interpreter"
-overpass_query = """
-[out:json];
-area[name="München"];
-nwr(area)[amenity~"^(bar|cafe|pub|restaurant)$"];
-out center;
-"""
-
-try:
-    response = requests.get(overpass_url, params={'data': overpass_query})
-    # Check if Server response is 200 = OK for the API call
-    print("Server-Code: ", response.status_code)
-    if(response.status_code != 200):
-        if(response.status_code == 504):
-            print("Server-Gateway-Timeout")
-            sys.exit()
-        elif(response.status_code == 429):
-            print("Too Many Requests")
-            sys.exit()
+# TODO Pandas Dataframe push to Database XAMPP!
+def get_amenity_info(area,amenity_type):
+    """
+    get_amenity_info: returns information about all amenities
+                      in a specific area
+    :param area:         str:
+                         city
+    :param amenity_type: str:
+                         bar,restaurant,cafe
+    :return pandas dataframe: df    if everything worked fine
+                         int: -1    if something went wrong
+    """
+    struct = {}
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = '[out:json];area[name="'+area+'"];' \
+                     'nwr(area)[amenity~"^('+amenity_type+')$"];' \
+                     'out center;'
+    try:
+        response = requests.get(overpass_url, params={'data': overpass_query})
+        # Check if Server response is 200 = OK for the API call
+        print("Server-Code: ", response.status_code)
+        if(response.status_code != 200):
+            if(response.status_code == 504):
+                print("Server-Gateway-Timeout")
+                return -1
+            elif(response.status_code == 429):
+                print("Too Many Requests")
+                return -1
+            elif (response.status_code == 400):
+                print("Bad Request! Check function Parameters")
+                return -1
+            else:
+                print("Server-Problems try later again!")
+                return -1
         else:
-            print("Server-Problems try later again!")
-            sys.exit()
-    else:
-        data = response.json()
-        # Flatten data and transport to pandas df
-        df = pd.json_normalize(data, record_path=['elements'])
-        df = df[['id', 'tags.name', 'lat', 'lon', 'tags.amenity', 'tags.opening_hours', 'tags.website',
-                 'tags.contact:website','tags.website_1', 'tags.website2', 'tags.phone', 'tags.cuisine',
-                 'tags.cuisine_1', 'tags.cuisine_2']]
-        print(df)
-        df.info()
-        df.to_csv('data_tables/muc_bars.csv')
+            print("Server: OK!")
+            data = response.json()
+            # Flatten data and transport to pandas df
+            df = pd.json_normalize(data, record_path=['elements'])
+            df = df[['id', 'tags.name', 'lat', 'lon', 'tags.amenity', 'tags.opening_hours', 'tags.website',
+                     'tags.contact:website','tags.website_1', 'tags.website2', 'tags.phone', 'tags.cuisine',
+                     'tags.cuisine_1', 'tags.cuisine_2']]
+            print("")
+            print("pandas_df_info: ")
+            print("########################################################################################")
+            df.info()
+            print("########################################################################################")
+            df.to_csv('data_tables/muc_bars_restaurants_cafes.csv')
+            return df
 
+    except Exception as e:
+        print("Unkown Failure is occured See Error Message!:")
+        print(e)
+        return -1
 
-except Exception as e:
-    print("Unkown Failure is occured See Error Message!:")
-    print(e)
-
-
-
+# call API
+get_amenity_info("München", "bar|cafe|pub|restaurant")
