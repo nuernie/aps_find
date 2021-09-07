79e4bcd1924d0633bb8673e5b6c8f246cbe2a00d
nuernie <nerdhub@web.de>
2021-09-07 08:35:10 +0200

    v 0.0.0

--- crawler/database_conn.py
+++ crawler/database_conn.py
@@ -3,12 +3,19 @@ import sqlalchemy
 from crawler.db_login import *
 from sqlalchemy.exc import SQLAlchemyError
 
+# TODO: Differenciate if Database is accessed externally or via
+#       local host
+# TODO: Error Handling check if Data is writen to db or is accesible
+# TODO: Server auf IONOS mieten und da die Datenbank aufziehen
+#       Vertragslaufzeit 12 Monate / 30 Tage geldzurück garantie
+#       Vorher den Vertrag kündigen! Mal ausprobieren!
 
 
-df = pd.DataFrame({'num_legs': [12, 4, 8, 18],
+df = pd.DataFrame({'num_legs': [13, 4, 8, 18],
                    'num_wings': [2, 0, 0, 0],
                    'num_specimen_seen': [10, 2, 1, 8]})
 print(df)
+# write to database
 try:
     database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                                    format(database_username, database_password,
@@ -17,3 +24,9 @@ try:
 except SQLAlchemyError as e:
     error = str(e.__dict__['orig'])
     print(error)
+
+# read from database
+df_sql = pd.read_sql_table('locality_info', database_connection)
+print(df_sql)
+# 1 vCore CPU 3GHz ohne graphische oberfläche!
+# vnc console vnc viewer / 
